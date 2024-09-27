1184e2b6e7171f9449e6eb00f112f4814db8a94e
nishantHolla <nishantholla19@gmail.com>
2024-09-27 23:16:33 +0530

    Add options

---
+++ lua/config/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+opt.colorcolumn = "100"
+opt.cursorline = true
+opt.cursorlineopt = "number"
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
+
+opt.confirm = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.swapfile = false
+opt.undofile = true
+opt.wrap = false
+
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.tabstop = 2
