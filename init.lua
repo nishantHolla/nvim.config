c0fc934254a898be41d14427d332839cfce1d3fc
nishantHolla <nishantholla19@gmail.com>
2024-09-27 23:29:09 +0530

    Add keymaps

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("config.options")
 require("config.autocommands")
+require("config.keymaps")
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,29 @@
+local set = vim.keymap.set
+local opt = function (desc, silent, noremap)
+  if silent == nil then silent = true end
+  if noremap == nil then noremap = true end
+
+  return {silent = silent, noremap = noremap, desc = desc}
+end
+
+vim.g.mapleader = ","
+
+set("n", "<esc>", ":nohl<cr>", opt("Clear highlights"))
+
+set({"i", "v"}, "<leader><leader>", "<esc>", opt("Go to normal mode"))
+
+set("i", "<M-h>", "<left>", opt("Move left"))
+set("i", "<M-j>", "<down>", opt("Move down"))
+set("i", "<M-k>", "<up>", opt("Move up"))
+set("i", "<M-l>", "<right>", opt("Move right"))
+
+set({"n", "v"}, "d", "\"_d", opt("Delete without updating unnamed register"))
+set({"n", "v"}, "c", "\"_c", opt("Change without updating unnamed register"))
+set({"n", "v"}, "x", "\"_x", opt("Delete character without updating unnamed register"))
+
+set({"n", "v"}, "<leader>d", "d", opt("Delete with updating unnamed register"))
+set({"n", "v"}, "<leader>c", "c", opt("Change with updating unnamed register"))
+set({"n", "v"}, "<leader>x", "x", opt("Delete character with updating unnamed register"))
+
+set("n", "<M-i>", ":bp<cr>", opt("Go to previous buffer"))
+set("n", "<M-o>", ":bn<cr>", opt("Go to next buffer"))
